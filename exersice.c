6136e01a00955d23871018e5cfa8a176184ed660
vatsal2004 <153715547+Vastal633@users.noreply.github.com>
2024-01-17 10:03:16 +0530

    Update exersice.c

--- exersice.c
+++ exersice.c
@@ -1,4 +1,4 @@
-//...............................................................Q1 read n numbers and prints its evarage....................................................................................
+//...............................................................Q1 read n numbers and prints its evarage................................................................................
 #include<stdio.h>
 int main(){
 	float n,sum=0;
@@ -17,5 +17,68 @@ int main(){
 }
 
 
-//...............................................................Q1 read n numbers and prints its evarage....................................................................................
+//...............................................................Q2 find num that divided by 5 and 7......................................................................................
 
+#include<stdio.h>
+
+int main(){
+	int n,divisable_num,garbage_num;
+	printf("enter the value of n: ");
+	scanf("%d",&n);
+
+	for(int i=1; i<n;i++){
+		if(i%5==0 || i%7==0){
+			divisable_num = i;
+			printf("%d\n",divisable_num);
+			
+		}
+		
+		else{
+			garbage_num = i;	
+		}	
+    }
+	return 0;
+}
+
+//..............................................................................Q3 print sum of digits...................................................................................
+
+#include<stdio.h>
+
+int main(){
+	int n,sum=0,digit;
+	printf("enter the value of n: ");
+	scanf("%d",&n);
+	
+	while(n!=0){
+		digit = n%10;
+		sum = sum + digit;
+		n = n/ 10;
+	}	
+	
+	printf("the sum of digiti is: %d",sum);
+	return 0;
+}
+//........................................................Q4 print the sum of odd numgers and print all odd numbers........................................................................
+
+#include<stdio.h>
+
+int main(){
+	int n,odd_num,even_num,sum=0;
+	printf("enter the value of n: ");
+	scanf("%d",&n);
+	
+	for(int i=0;i<=n;i++){
+		if(i%2!=0){
+			odd_num = i;
+			sum = sum + i;
+			printf(" %d ",odd_num);
+		}
+		
+		else{
+			even_num= i;
+		}
+	}
+	
+	printf("\nthe sum of odd digits is: %d",sum);
+	return 0;
+}
